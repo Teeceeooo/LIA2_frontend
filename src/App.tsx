1ae136c837059b566a18f9578f439d558d0f607e
Niqqo93 <sebastian.granlund@live.se>
2024-01-25 14:40:56 +0100

    Participant komponent

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import "./App.css";
 import PersistentDrawerLeft from "./components/mainmenu";
 import QrScanner from "./components/qrscanner";
+import Participant from "./components/participant";
 import Footer from "./components/footer";
 
 function App() {
   return (
     <div className="App">
       <PersistentDrawerLeft />
-      <QrScanner />
+      <Participant />
     </div>
   );
 }
