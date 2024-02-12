998e6881439c744c98664a9d2d3012f707f6869c
Sebastian Granlund <sebastian.granlund@live.se>
2024-02-12 15:12:36 +0100

    Borttagning av onödigt som inte användes.

--- src/components/createparticipant.tsx
+++ src/components/createparticipant.tsx
@@ -1,5 +1,4 @@
 import { Button, TextField } from "@mui/material";
-import axios from "axios";
 import React, { useState } from "react";
 import addParticipant from "../api/postparticipantapi";
 import { useParams } from "react-router-dom";
--- src/components/qrscanner.tsx
+++ src/components/qrscanner.tsx
@@ -1,22 +1,12 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { Html5QrcodeScanner } from "html5-qrcode";
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { Outlet, Link } from "react-router-dom";
-import Participant from "./participant";
-import { Switch } from "@mui/material";
-import { Route, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function QrScanner() {
   let scanner: Html5QrcodeScanner | null = null;
-  const [QRresult, setQRresult] = useState<Participant | null>(null);
   const navigate = useNavigate();
-
-  interface Participant {
-    fullName: string;
-    image: {
-      imageUrl: string;
-    };
-  }
 
   useEffect(() => {
     if (!scanner) {
